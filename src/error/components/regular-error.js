55fb880b42faa80b44047b15af4befef21bac05e
Steven <stevenbdf@gmail.com>
2019-03-17 15:30:48 -0600

    deploy

--- src/error/components/regular-error.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const regularError = (props) => {
-    return(
-        <img src={'../../../images/error.png'} width={'100%'} />
-    )
-}
-
-export default regularError
